dced08afd7bef5bc676009b2a24199253851dcd4
Oussama Ajala <ajala1713@gmail.com>
2025-10-22 15:25:44 -0400

    removing .next file from tracking

--- frontend/.next/static/chunks/app_login_page_tsx_4e845878._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_9cc84623._.js"
-],
-    source: "dynamic"
-});
--- frontend/.next/static/chunks/app_signup_page_tsx_4e845878._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_signup_page_tsx_5fe4dbb2._.js"
-],
-    source: "dynamic"
-});
